3455b067d01955bddf2307e80a2714c3c89c11a2
Pie1981 <plazzapietro81@gmail.com>
2022-01-25 10:56:22 +0100

    Create somma e media.js
    Ecco il mio codice per l esercizio somma e media. Ho cercato di pensare a qualcosa di meno lungo da scrivere ma dovendo definire i 5 numeri credo che possa essere corretto questo sistema. Se qualcuno ha dei consigli che scriva pure.

---
+++ esercizi-stringhe-numeri/somma e media.js
@@ -0,0 +1,11 @@
+var a = 25;
+var b = 5;
+var c = 10;
+var d = 3;
+var e = 7;
+
+var numTot = 5;
+var sum = (a+b+c+d+e);
+var diff = sum/numTot;
+
+console.log(`abbiamo ${numTot} numeri totali che sommati ci daranno ${sum} mentre la loro media risulterà ${diff})`);
