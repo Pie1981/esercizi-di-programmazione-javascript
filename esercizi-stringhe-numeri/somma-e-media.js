c196fdd46f4fad4e366e2823abc99709b7a6cceb
Pie1981 <plazzapietro81@gmail.com>
2022-01-25 10:37:44 +0100

    Soluzione somma e media
    Ecco il mio codice per l esercizio somma e media. Ho cercato di pensare a qualcosa di meno lungo da scrivere ma dovendo definire i 5 numeri credo che possa essere corretto questo sistema. Se qualcuno ha dei consigli che scriva pure.

--- esercizi-stringhe-numeri/somma-e-media.js
+++ esercizi-stringhe-numeri/somma-e-media.js
@@ -5,6 +5,17 @@
   Esempio:
     Input: a = 1, b = 2, c = 3, d = 4, e = 5
     Output: somma = 15, media = 3
-  
+
   http://www.imparareaprogrammare.it
 */
+var a = 25;
+var b = 5;
+var c = 10;
+var d = 3;
+var e = 7;
+
+var numTot = 5;
+var sum = (a+b+c+d+e);
+var diff = sum/numTot;
+
+console.log(`abbiamo ${numTot} numeri totali che sommati ci daranno ${sum} mentre la loro media risulterà ${diff})`);
