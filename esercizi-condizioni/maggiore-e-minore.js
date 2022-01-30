428ef456354c62759caddd2de05fd08ce588ba49
Pie1981 <plazzapietro81@gmail.com>
2022-01-30 17:32:28 +0100

    soluzione
    Spero che questa soluzione sia quella corretta e piu funzionale all esercizio.
    Ho cercato di pensare ad una soluzione di codice piu breve ma non ci sono riuscito. Qualora dovesse esserci e gradito un feedback. grazie

--- esercizi-condizioni/maggiore-e-minore.js
+++ esercizi-condizioni/maggiore-e-minore.js
@@ -6,7 +6,28 @@
   Esempio:
     Input: a = 3, b = -1, c = 4, d = -2
     Output: maggiore = 4, minore = -2
-    
+
 
   http://www.imparareaprogrammare.it
 */
+
+var a = 3, b = -1, c = 4, d = -2;
+
+if (a >=b && a>=c && a>=d)
+ console.log('Maggiore'+a);
+else if (b>=a && b>=c && b>=d) {
+  console.log('Maggiore'+ b);
+}else if (c>=a && c>=b && c>=d) {
+  console.log('Maggiore'+c);
+}else{
+  console.log('Maggiore'+d);
+}
+if (a<=b && a<=c && a<=d) {
+  console.log('Minore'+a);
+}else if (b<=a && b<=c && b<=d) {
+  console.log('Minore'+b);
+}else if (c<=a && c<=b && c<=d) {
+  console.log('Minore'+c);
+}else {
+  console.log('Minore'+d);
+}
