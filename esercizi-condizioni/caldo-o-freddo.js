aa2bc788038ef8319619ca53fe6802770e8c6d13
Pie1981 <plazzapietro81@gmail.com>
2022-02-04 14:48:09 +0100

    Caldo o freddo
    Soluzione all esercizio Caldo o freddo

--- esercizi-condizioni/caldo-o-freddo.js
+++ esercizi-condizioni/caldo-o-freddo.js
@@ -9,3 +9,36 @@
 
   http://www.imparareaprogrammare.it
 */
+var temp = a = 10, b = -2, c = 31, d = 22, e = 15, f = -6 , g = 7;
+
+if(a>=b && a>=c && a>=d && a>=e && a>=f && a>=g)
+console.log('Giornata piu calda' + a);
+else if (b>=a && b>=c && b>=d && b>=e && b>=f && b>=g){
+  console.log('Giornata piu calda' + b);
+}else if (c>=a && c>=b && c>=d && c>=e && c>=f && c>=g) {
+  console.log('Giornata piu calda' + c);
+}else if (d>=a && d>=b && d>=c && d>=e && d>=f && d>=g) {
+  console.log('Giornata piu calda' + d);
+}else if (e>=a && e>=b && e>=c && e>=d && e>=f && e>=g) {
+console.log('Giornata piu calda' + e);
+}else if (f>=a && f>=b && f>=c && f>=d && f>=e && f>=g) {
+  console.log('Giornata piu calda' + f);
+}else {
+  console.log('Giornata piu calda' + g);
+}
+if (a<=b && a<=c && a<=d && a<=e && a<=f && a<=g) {
+  console.log('Giornata piu fredda' + a);
+}else if (b<=a && b<=c && b<=d && b<=e && b<=f && b<=g){
+  console.log('Giornata piu fredda' + b);
+}else if (c<=a && c<=b && c<=d && c<=e && c<=f && c<=g) {
+  console.log('Giornata piu fredda' + c);
+}else if (d<=a && d<=b && d<=c && d<=e && d<=f && d<=g) {
+  console.log('Giornata piu fredda' + d);
+}else if (e<=a && e<=b && e<=c && e<=d && e<=f && e<=g) {
+console.log('Giornata piu fredda' + e);
+}else if (f<=a && f<=b && f<=c && f<=d && f<=e && f<=g) {
+  console.log('Giornata piu fredda' + f); 
+
+}else {
+  console.log('Giornata piu fredda' + g);
+}
