096f9f36bb486d1bc3cc3327a64e6b762a688d1f
Pie1981 <plazzapietro81@gmail.com>
2022-02-22 18:10:28 +0100

    Rombo che stampa

--- esercizi-cicli/rombo-che-stampa.js
+++ esercizi-cicli/rombo-che-stampa.js
@@ -13,6 +13,18 @@
 
   Consiglio:
   Per indentare puoi usare il tab con '\t'.
-  
+
   http://www.imparareaprogrammare.it
 */
+var primaRiga = ' *';
+ var secRiga = '';
+ var terzRiga = '';
+
+ for( a = 1; a <= 3; a++){
+   secRiga+='*';
+ }
+ for( a = 1; a <= 5; a++){
+   terzRiga+='*';
+ }
+
+ console.log(' \t\t ' + primaRiga + '\n' + ' \t\t ' + secRiga + '\n' + '\t\t' + terzRiga + '\n' + ' \t\t ' + secRiga + '\n' + ' \t\t ' + primaRiga);
